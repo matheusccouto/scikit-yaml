224cf5f7f8adfd0f87d3f316b275af0e715c5399
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-07-05 00:04:32 +0000

    Fix name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(THIS_DIR, "requirements.txt"), encoding="utf-8") as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-    name="skyaml",
+    name="scikit-yaml",
     version="0.1.0",
     author="Matheus Couto",
     author_email="matheusccouto@gmail.com",
